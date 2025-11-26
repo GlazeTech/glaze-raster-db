31063bda018e18c855076f9f70aa9ac9da527d71
Bjørn Mølvig <bjoernmoelvig@gmail.com>
2025-11-26 16:15:48 +0100

    copilot; circular import

--- grdb/devtools.py
+++ grdb/devtools.py
@@ -12,7 +12,7 @@ import time
 import uuid
 from typing import TYPE_CHECKING
 
-from grdb import add_pulses, create_db
+from grdb.crud import add_pulses, create_db
 from grdb.models import (
     AxesMapping,
     AxisMap,
